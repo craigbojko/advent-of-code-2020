7d897447304f5b5341b757193d0de0682ea35534
Craig Bojko <craig.bojko@trint.com>
2021-01-14 16:41:51 +0000

    feat(aoc): implemented day 6 - part 2

--- 6/index.ts
+++ 6/index.ts
@@ -5,16 +5,20 @@ const FILENAME = 'data.txt'
 
 interface Group {
   lines: string[];
-  answerSet: Set<string>;
-  answerSetUniqueSum: number;
+  unionSet: Set<string>;
+  unionSetSum: number;
+  intersectionSet: Set<string>;
+  intersectionSetSum: number;
 }
 
 const getNextGroup = (reader: FileReader) => {
   let line: string | null
   let group: Group = {
     lines: [],
-    answerSet: new Set(),
-    answerSetUniqueSum: -1
+    unionSet: new Set(),
+    unionSetSum: -1,
+    intersectionSet: new Set(),
+    intersectionSetSum: -1,
   }
   do {
     line = reader.nextLine()
@@ -23,41 +27,58 @@ const getNextGroup = (reader: FileReader) => {
     }
 
     if (line?.length ?? 0 < 1) { // End of Group
-      const chars: string[] = line?.split('') ?? []
-      chars.forEach(char => group.answerSet.add(char))
-      group.answerSetUniqueSum = group.answerSet.size
-    } else {
       // Add line to group
       group.lines.push(line)
+
+      // Part 1
+      const chars: string[] = line?.split('') ?? []
+      chars.forEach(char => group.unionSet.add(char))
+      group.unionSetSum = group.unionSet.size
+
+      // Part 2
+      const lineArr: string[][] = group.lines.map(line => line.split(''))
+      let intersectionA: string[] = lineArr.shift()!
+      let intersectionB: string[] | undefined
+      while ((intersectionB = lineArr.shift()) !== undefined) {
+        intersectionA = intersectionA.filter(char => intersectionB!.includes(char))
+      }
+      group.intersectionSet = new Set(intersectionA)
+      group.intersectionSetSum = group.intersectionSet.size
     }
   } while (line?.length)
   return group
 }
 
-const part1 = async (): Promise<number> => {
+const run = async (): Promise<[number, number]> => {
   const reader = new FileReader(`${__dirname}/${FILENAME}`)
 
   const groups: Group[] = []
   let group: Group | null
-  let groupAnswerSum: number = 0
+  let groupUnionSum: number = 0
+  let groupIntersectionSum: number = 0
+
   // Read all groups into array
   while ((group = getNextGroup(reader)) !== null) {
     groups.push(group)
-    groupAnswerSum += group.answerSetUniqueSum
+    groupUnionSum += group.unionSetSum
+    groupIntersectionSum += group.intersectionSetSum
   }
 
+  console.debug('---')
   console.debug(`Number of Groups: ${groups.length}`)
-  console.debug(`Group size sum: ${groupAnswerSum}`)
-  return groupAnswerSum
+  console.debug(`Group union size sum: ${groupUnionSum}`)
+  console.debug(`Group intersection size sum: ${groupIntersectionSum}`)
+  console.debug('---')
+  return [groupUnionSum, groupIntersectionSum]
 }
 
 const solution: Solution = {
-  part1: () => part1().then((result: number) => {
-    console.log(`Result: 1 = ${JSON.stringify(result)}`)
+  part1: () => run().then((result: [number, number]) => {
+    console.log(`Result: 1 = ${JSON.stringify(result[0])}`)
   }),
-  // part2: () => part2().then((result: number) => {
-  //   console.log(`Result: 2 = ${JSON.stringify(result)}`)
-  // })
+  part2: () => run().then((result: [number, number]) => {
+    console.log(`Result: 2 = ${JSON.stringify(result[1])}`)
+  })
 }
 
 export default solution
