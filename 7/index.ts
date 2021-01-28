41f3c0ca9fc68a81d7518936e65c03ccd2936cfe
Craig Bojko <craig.bojko@trint.com>
2021-01-28 13:48:09 +0000

    feat(aoc): implemented day 7 - part 2

--- 7/index.ts
+++ 7/index.ts
@@ -2,9 +2,7 @@ import type { Solution } from '../index'
 import FileReader from '../helpers/FileReader.class'
 
 const DEBUG = false
-
 const FILENAME = 'data.txt'
-// const FILENAME = 'debug.txt'
 
 type Bag = {
   colour: string;
@@ -17,6 +15,28 @@ type InnerBag = Bag & {
 type Rule = {
   container: Bag;
   contents: Array<InnerBag | false>;
+}
+
+type HashTable = {
+  [key: string]: InnerBag[];
+}
+
+const readDataFile = (): Rule[] => {
+  const reader = new FileReader(`${__dirname}/${FILENAME}`)
+  const rules: Rule[] = []
+  let line: string | null
+
+  // Read file and build ruleset
+  do {
+    line = reader.nextLine()
+    if (line?.length) {
+      const rule: Rule | null = parseLine(line)
+      rule && rules.push(rule)
+    }
+  }
+  while (line?.length)
+
+  return rules
 }
 
 const parseLine = (line: string): Rule | null => {
@@ -54,6 +74,34 @@ const parseLine = (line: string): Rule | null => {
   }
 }
 
+const buildHashTable = (rules: Rule[]): HashTable => {
+  return rules.reduce((table: HashTable, rule: Rule) => {
+    const innerBags: InnerBag[] = (rule.contents.length && rule.contents[0] !== false) ? <InnerBag[]>rule.contents : []
+    return {
+      ...table,
+      [`${rule.container.variant} ${rule.container.colour}`]: innerBags
+    }
+  }, {} as HashTable)
+}
+
+const recursiveCount = (hashTable: HashTable, bagType: string): number => {
+    let count: number = 0
+    let bag: InnerBag[] = hashTable[bagType]
+    
+    if (bag.length == 0) {
+      return count
+    } else {
+      bag.forEach((subBag: InnerBag) => {
+        const type = `${subBag.variant} ${subBag.colour}`
+        if (type == 'shiny gold') {
+          count += 1
+        }
+        count += recursiveCount(hashTable, type)
+      })
+    }
+    return count
+}
+
 const filterByInnerBag = (bag: Bag, ruleSet: Rule[]): Bag[] => {
   const rules: Rule[] = ruleSet.filter((rule: Rule) =>
     rule.contents.filter((inner: InnerBag | false) => {
@@ -67,22 +115,21 @@ const filterByInnerBag = (bag: Bag, ruleSet: Rule[]): Bag[] => {
   return rules.map(rule => rule.container)
 }
 
-const run = async (): Promise<number> => {
-  const reader = new FileReader(`${__dirname}/${FILENAME}`)
-  const rules: Rule[] = []
-  let line: string | null
+const run1Recursive = async (rules: Rule[]): Promise<number> => {
+  const hashTable: HashTable = buildHashTable(rules)
+  // console.log(JSON.stringify(hashTable, null, 2))
 
-  // Read file and build ruleset
-  do {
-    line = reader.nextLine()
-    if (line?.length) {
-      const rule: Rule | null = parseLine(line)
-      rule && rules.push(rule)
+  let count: number = 0
+  Object.keys(hashTable).forEach((bag: string) => {
+    if (recursiveCount(hashTable, bag) > 0) {
+      // console.log(`${bag} bags contain at least one shiny gold bag!`)
+      count += 1
     }
-  }
-  while (line?.length)
+  })
+  return count
+}
 
-  // Loop through rules and calculate
+const run1Iterative = async (rules: Rule[]): Promise<number> => {
   const ourBag: Bag = { colour: 'gold', variant: 'shiny' }
   const setOfBags: Set<Bag> = new Set()
   const queue: Bag[] = [ourBag]
@@ -107,13 +154,55 @@ const run = async (): Promise<number> => {
   return setOfBags.size
 }
 
+const run1 = async (): Promise<[number, number]> => {
+  const rules: Rule[] = readDataFile()
+
+  const iterativeResult = await run1Iterative(rules)
+  const recursiveResult = await run1Recursive(rules)
+
+  return [
+    iterativeResult,
+    recursiveResult,
+  ]
+}
+
+const run2 = async (): Promise<number> => {
+  const rules: Rule[] = readDataFile()
+  const ourBag: Bag = { colour: 'gold', variant: 'shiny' }
+  const queue: Bag[] = [ourBag]
+
+  let currentBag: Bag | undefined
+  let runningCount: number = 0
+
+  while ((currentBag = queue.shift()) !== undefined) {
+    const currentBagRule: Rule | undefined = rules.find((rule: Rule) => rule.container.colour === currentBag?.colour && rule.container.variant === currentBag?.variant)
+    if (currentBagRule) {
+      const innerBags: InnerBag[] = currentBagRule.contents.filter(bag => bag !== false) as InnerBag[]
+
+      innerBags.forEach((innerBag: InnerBag | false) => {
+        if (innerBag) {
+          runningCount += innerBags.length && innerBag.quantity
+          for (let i=0; i<innerBag.quantity; i+=1) {
+            queue.push({
+              colour: innerBag.colour,
+              variant: innerBag.variant
+            })
+          }
+        }
+      })
+    }
+  }
+
+  return runningCount
+}
+
 const solution: Solution = {
-  part1: () => run().then((result: number) => {
-    console.log(`Result: 1 = ${JSON.stringify(result)}`)
+  part1: () => run1().then((result: [number, number]) => {
+    console.log(`Result: 1 = ${JSON.stringify(result[0])}`)
   }),
-  // part2: () => run().then((result: [number, number]) => {
-  //   console.log(`Result: 2 = ${JSON.stringify(result[1])}`)
-  // })
+  part2: () => run2().then((result: number) => {
+    console.log(`Result: 2 = ${JSON.stringify(result)}`)
+  })
 }
 
 export default solution
